44129288ac5a9b7a4416b31c255298f6196eb5c9
Xiaofei <xiaofei.it@gmail.com>
2016-05-29 00:36:42 +0800

    Fix a small bug in CodeUtils

--- android-data-storage/src/main/java/xiaofei/library/datastorage/util/CodeUtils.java
+++ android-data-storage/src/main/java/xiaofei/library/datastorage/util/CodeUtils.java
@@ -61,7 +61,7 @@ public class CodeUtils {
     }
 
     private static int charToInt(char ch) {
-        return 0 <= ch && ch <= '9' ? ch - '0' : ch - 'A' + 10;
+        return '0' <= ch && ch <= '9' ? ch - '0' : ch - 'A' + 10;
     }
 
     public static byte[] decodeToBytes(String input) {
